87298dc32d5ff851b53d9245643c9589371a678b
Aditya Dutta <adityadtt4@gmail.com>
2026-07-23 16:12:31 +0100

    promising tests

---
+++ LR.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+from sklearn.preprocessing import minmax_scale
+from sklearn.model_selection import train_test_split
+
+
+df = pd.read_excel('testing.xlsx')
+df = df.drop(columns=['Player Name','Club','Nationality','Hit Woodwork','Offsides'])
+
+df_new = df.copy()
+
+
+
+df_new['Market Value (EUR)'] = minmax_scale(df_new['Market Value (EUR)'])
+
+
+df_in = df_new.drop(columns=['Market Value (EUR)'])
+df_out = df_new['Market Value (EUR)']
+
+x_train, x_test, y_train, y_test = train_test_split(df_in,df_out,test_size=0.2,random_state=1)
+
+lr = LinearRegression()
+lr.fit(x_train,y_train)
+
+y_pred = lr.predict(x_train)
+
+r2 = r2_score(y_train,y_pred)
+
+print(f"R2: {r2}")
